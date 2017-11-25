7ee46d2ac00ea63bffbedc6a4a6242413444004f
fono09 <fono09@users.noreply.github.com>
2017-11-25 16:41:42 +0900

    Edit env.sh

--- home/isucon/env.sh
+++ home/isucon/env.sh
@@ -2,4 +2,4 @@ ISU_DB_HOST=app0254
 ISU_DB_USER=isucon
 ISU_DB_PASSWORD=isucon
 ISU_WEB_HOSTS=app0251.isu7f.k0y.org,app0252.isu7f.k0y.org,app0253.isu7f.k0y.org
-#ISU_DB_SOCK=/var/run/mysqld/mysqld.sock
+ISU_DB_SOCK=/var/run/mysqld/mysqld.sock
